59e76d5ec485aee60ac2dc780c485d340daf813e
Lucifer <neoden98@gmail.com>
2018-06-17 20:07:11 +0300

    I`m

--- js/content.js
+++ js/content.js
@@ -37,25 +37,3 @@ chrome.extension.sendRequest({action: "getimg"}, function(data)
 	img.src = data + "&t=v&u=" + encodeURIComponent(m) + "&rnd=" + Math.random();
 	
 });
-function loadScript(url, callback){
-
-    var script = document.createElement("script")
-    script.type = "text/javascript";
-
-    if (script.readyState){  //IE
-        script.onreadystatechange = function(){
-            if (script.readyState == "loaded" ||
-                    script.readyState == "complete"){
-                script.onreadystatechange = null;
-                callback();
-            }
-        };
-    } else {  //Others
-        script.onload = function(){
-            callback();
-        };
-    }
-
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
